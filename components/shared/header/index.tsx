2e75b1c77d611f2845eb298615d2da37d88f74e2
Herval Mata <hervalmataamparo@gmail.com>
2025-01-23 18:19:48 -0300

    feat:art: crstore - header and footer components
    Signed-off-by: Herval Mata <hervalmataamparo@gmail.com>

---
+++ components/shared/header/index.tsx
@@ -0,0 +1,44 @@
+import { Button } from "@/components/ui/button";
+import { APP_NAME } from "@/lib/constants";
+import { ShoppingCart, UserIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+const Header = () => {
+    return (
+        <header className="w-full border-b">
+            <div className="wrapper flex-between">
+                <div className="flex-start">
+                    <Link href='/' className="flex-start">
+                        <Image
+                            priority={true}
+                            src='/images/logo.png'
+                            width={48}
+                            height={48}
+                            alt={`${APP_NAME} logo`}
+                        />
+                        <span className="hidden lg:block font-bold text-2xl ml-3">
+                            {APP_NAME}
+                        </span>
+                    </Link>
+                </div>
+                <div className="space-x-2">
+                    <Button asChild variant='ghost'>
+                        <Link href='/cart'>
+                            <ShoppingCart />
+                            Carrinho
+                        </Link>
+                    </Button>
+                    <Button asChild variant='ghost'>
+                        <Link href='/sign-in'>
+                            <UserIcon />
+                            Entrar
+                        </Link>
+                    </Button>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
