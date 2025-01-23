565a813a98e541400c5f328d563533def0f4144b
Herval Mata <hervalmataamparo@gmail.com>
2025-01-23 17:48:10 -0300

    feat:art: crstore - root layout and constants
    Signed-off-by: Herval Mata <hervalmataamparo@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
+import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "CRStore",
-  description: "A modern store built ith Next.js, ShadCN, and Prisma.",
+  title: {
+    template: `%s | ${APP_NAME}`,
+    default: APP_NAME,
+  },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL)
 };
 
 export default function RootLayout({
@@ -15,9 +20,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${inter.className} antialiased`}
+        className={`${inter.className}`}
       >
         {children}
       </body>
