200809ee3c67f9f09631935d0a17adea99b9d7ea
Herval Mata <hervalmataamparo@gmail.com>
2025-01-23 21:23:11 -0300

    feat:art: crstore - list sample products
    Signed-off-by: Herval Mata <hervalmataamparo@gmail.com>

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,16 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 const delay = (ms: number | undefined) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const HomePage = async () => {
   await delay(2000);
-  return <>Últimos Produtos</>
+  return (
+      <div className='space-y-8'>
+        <h2 className='h2-bold'>Últimos Produtos</h2>
+        <ProductList title='Produtos Novos' data={sampleData.products} limit={4} />
+      </div>
+  )
 }
 
 export default HomePage;
