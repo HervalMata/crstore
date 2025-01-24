e1b3ac7364d99846c29b9e7d2f1d4d9aad691913
Herval Mata <hervalmataamparo@gmail.com>
2025-01-24 18:34:43 -0300

    feat:art: crstore - product images component
    Signed-off-by: Herval Mata <hervalmataamparo@gmail.com>

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -5,6 +5,7 @@ import ProductPrice from "@/components/shared/product/product-price";
 import {Card, CardContent} from "@/components/ui/card";
 import {Badge} from "@/components/ui/badge";
 import {Button} from "@/components/ui/button";
+import ProductImages from "@/components/shared/product/product-images";
 
 const ProductDetailsPage = async (props: {
     params: Promise<{ slug: string  }>;
@@ -19,7 +20,9 @@ const ProductDetailsPage = async (props: {
         <>
             <section>
                 <div className="grid grid-cols-1 md:grid-cols-5">
-                    <div className='col-span-2'></div>
+                    <div className='col-span-2'>
+                        <ProductImages images={product.images} />
+                    </div>
                     <div className='col-span-2 p-5'>
                         <div className='flex flex-col gap-6'>
                             <p>
