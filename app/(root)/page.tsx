eb3e5de64d82cde8f953d11caba9c4de54bc9dd1
Herval Mata <hervalmataamparo@gmail.com>
2025-01-24 15:41:57 -0300

    feat:art: crstore - load products from database
    Signed-off-by: Herval Mata <hervalmataamparo@gmail.com>

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,18 @@
 import ProductList from "@/components/shared/product/product-list";
-import sampleData from "@/db/sample-data";
+import {getLatestProducts} from "@/lib/actions/product.actions";
 
 const delay = (ms: number | undefined) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const HomePage = async () => {
-  await delay(2000);
-  return (
-      <div className='space-y-8'>
-        <h2 className='h2-bold'>Últimos Produtos</h2>
-        <ProductList title='Produtos Novos' data={sampleData.products} limit={4} />
-      </div>
-  )
-}
+    const latestProducts = await getLatestProducts();
+    await delay(2000);
+
+    return (
+        <div className='space-y-8'>
+            <h2 className='h2-bold'>Últimos Produtos</h2>
+            <ProductList title='Produtos Novos' data={latestProducts} />
+        </div>
+    );
+};
 
 export default HomePage;
