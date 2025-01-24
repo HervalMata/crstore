9e08eeb486ce5f7a4e1e5ec6f9a88e51a7b3a587
Herval Mata <hervalmataamparo@gmail.com>
2025-01-24 16:34:00 -0300

    feat:art: crstore - zod validation and type inference
    Signed-off-by: Herval Mata <hervalmataamparo@gmail.com>

---
+++ lib/validator.ts
@@ -0,0 +1,20 @@
+import {z} from "zod";
+import {formatNumberWithDecimal} from "@/lib/utils";
+
+const currency = z.string().refine(
+    (value) => /^\d+(\.\d{2})?R$/.test(formatNumberWithDecimal(Number(value))),
+    'Preço deve ter exatamente 2 decimais'
+)
+
+export const insertProductSchema = z.object({
+    name: z.string().min(3, 'Nome deve ter pelo menos 3 caracteres'),
+    slug: z.string().min(3, 'Slug deve ter pelo menos 3 caracteres'),
+    category: z.string().min(3, 'Categoria deve ter pelo menos 3 caracteres'),
+    brand: z.string().min(3, 'Marca deve ter pelo menos 3 caracteres'),
+    description: z.string().min(3, 'Descrição deve ter pelo menos 3 caracteres'),
+    stock: z.coerce.number(),
+    images: z.array(z.string().min(3, 'Produto deve ter pelo menos uma imagem')),
+    isFeatured: z.boolean(),
+    banner: z.string().nullable(),
+    price: currency,
+})
