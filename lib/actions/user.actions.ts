2be8965ef7474c115cf141eaf644e047cf2d0d03
Herval Mata <hervalmataamparo@gmail.com>
2025-01-25 22:16:06 -0300

    feat:art: crstore - sign up zod schema and action
    Signed-off-by: Herval Mata <hervalmataamparo@gmail.com>

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,8 +1,10 @@
 'use server';
 
-import {signInFormSchema} from "@/lib/validator";
+import {signInFormSchema, signUpFormSchema} from "@/lib/validator";
 import {signIn, signOut} from "@/auth";
 import {isRedirectError} from "next/dist/client/components/redirect-error";
+import {hashSync} from "bcrypt-ts-edge";
+import {prisma} from "@/db/prisma";
 
 export async function signInWithCredentials(
     prevState: unknown,
@@ -28,4 +30,40 @@ export async function signInWithCredentials(
 
 export async function signOutUser() {
     await signOut();
+}
+
+export async function signUpUser(prevState: unknown, formData: FormData) {
+    try {
+        const user = signUpFormSchema.parse({
+            name: formData.get('name'),
+            email: formData.get('email'),
+            password: formData.get('password'),
+            confirmPassword: formData.get('confirmPassword'),
+        });
+
+        const plainPassword = user.password;
+
+        user.password = hashSync(user.password, 10);
+
+        await prisma.user.create({
+            data: {
+                name: user.name,
+                email: user.email,
+                password: user.password,
+            },
+        });
+
+        await signIn('credentials', {
+            email: user.email,
+            password: plainPassword,
+        });
+
+        return { success: true, message: 'Usuário cadastre com sucesso!' };
+    } catch (error) {
+        if (isRedirectError(error)) {
+            throw error;
+        }
+
+        return { success: false, message: 'Usuário não foi cadastrado!' };
+    }
 }
--- lib/validator.ts
+++ lib/validator.ts
@@ -21,5 +21,17 @@ export const insertProductSchema = z.object({
 
 export const signInFormSchema = z.object({
     email: z.string().email('Email invalido').min(3, 'Email deve ter pelo menos 3 caracteres'),
-    password: z.string().min(3, 'Password deve ter pelo menos 3 caracteres'),
+    password: z.string().min(3, 'Senha deve ter pelo menos 3 caracteres'),
 })
+
+export const signUpFormSchema = z.object({
+    name: z.string().min(3, 'Nome deve ter pelo menos 3 caracteres'),
+    email: z.string().email('Email invalido'),
+    password: z.string().min(6, 'Senha deve ter pelo menos 6 caracteres'),
+    confirmPassword: z.string().min(3, 'A confirmação de Senha deve ter pelo menos 6 caracteres'),
+}).refine(
+    (data) => data.password === data.confirmPassword, {
+        message: 'Senhas não conferem',
+        path: ['confirmPassword'],
+    }
+);
