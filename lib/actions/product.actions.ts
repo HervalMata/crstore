fe8c97db806727470f0209caed152a6c88afbbd1
Herval Mata <hervalmataamparo@gmail.com>
2025-02-20 14:28:27 -0300

    feat:art: crstore - get all products for admin
    Signed-off-by: Herval Mata <hervalmataamparo@gmail.com>

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -2,7 +2,7 @@
 
 import { prisma } from "@/db/prisma";
 import {convertToPlainObject} from "@/lib/utils";
-import {LATEST_PRODUCTS_LIMIT} from "@/lib/constants";
+import {LATEST_PRODUCTS_LIMIT, PAGE_SIZE} from "@/lib/constants";
 
 export async function getLatestProducts() {
 
@@ -20,4 +20,30 @@ export async function getProductBySlug(slug: string) {
         where: { slug: slug },
     });
 
+}
+
+export async function getAllProducts({
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    query,
+    limit = PAGE_SIZE,
+    page,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    category,
+} : {
+    query: string;
+    limit?: number;
+    page: number;
+    category?: string;
+}) {
+    const data = await prisma.product.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+    });
+
+    const dataCount = await prisma.product.count();
+
+    return {
+        data,
+        totalPages: Math.ceil(dataCount / limit),
+    }
 }
