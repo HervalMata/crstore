e07d206498173f527cc3eebd703bcb935df1bc51
Herval Mata <hervalmataamparo@gmail.com>
2025-02-21 15:59:35 -0300

    feat:art: crstore - product update form
    Signed-off-by: Herval Mata <hervalmataamparo@gmail.com>

---
+++ app/admin/products/[id]/page.tsx
@@ -0,0 +1,28 @@
+import {Metadata} from "next";
+import {getProductById} from "@/lib/actions/product.actions";
+import {notFound} from "next/navigation";
+import ProductForm from "@/app/admin/products/product-form";
+
+export const metadata: Metadata = {
+    title: "Atualizar Produto",
+};
+
+const AdminProductUpdatePage = async (props: {
+    params: Promise<{
+        id: string;
+    }>;
+}) => {
+    const { id } = await props.params;
+    const product = await getProductById(id);
+
+    if (!product) return notFound();
+
+    return (
+        <div className='space-y-8 max-w-5xl mx-auto'>
+            <h1 className='h2-bold'>Atualizar Produto</h1>
+            <ProductForm type= 'Update' product={product} productId={product.id} />
+        </div>
+    );
+};
+
+export default AdminProductUpdatePage;
