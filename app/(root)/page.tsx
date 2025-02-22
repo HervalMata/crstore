36e81e67d72e0c2653b51e666a60240dffe7d787
Herval Mata <hervalmataamparo@gmail.com>
2025-02-21 22:14:13 -0300

    feat:art: crstore - featured products carousel
    Signed-off-by: Herval Mata <hervalmataamparo@gmail.com>

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,22 @@
 import ProductList from "@/components/shared/product/product-list";
-import {getLatestProducts} from "@/lib/actions/product.actions";
+import {getFeaturedProducts, getLatestProducts} from "@/lib/actions/product.actions";
+import ProductCarousel from "@/components/shared/product/product-carousel";
 
 const delay = (ms: number | undefined) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const HomePage = async () => {
     const latestProducts = await getLatestProducts();
+    const featuredProducts = await getFeaturedProducts();
+
+
     await delay(2000);
 
     return (
         <div className='space-y-8'>
             <h2 className='h2-bold'>Últimos Produtos</h2>
+            {featuredProducts.length > 0 && (
+                <ProductCarousel data={featuredProducts} />
+            )}
             <ProductList title='Produtos Novos' data={latestProducts} />
         </div>
     );
