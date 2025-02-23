c778c704ffd9e682db31093c0ce2c727c4258b67
Herval Mata <hervalmataamparo@gmail.com>
2025-02-23 13:11:32 -0300

    feat:art: crstore - create and update review action
    Signed-off-by: Herval Mata <hervalmataamparo@gmail.com>

---
+++ lib/actions/review.actions.ts
@@ -0,0 +1,73 @@
+import {z} from "zod";
+import {insertReviewSchema} from "@/lib/validator";
+import {auth} from "@/auth";
+import {prisma} from "@/db/prisma";
+import {revalidatePath} from "next/cache";
+import {formatError} from "@/lib/utils";
+
+export async function createUpdateReview(
+    data: z.infer<typeof insertReviewSchema>
+) {
+    try {
+        const session = await auth();
+        if (!session) throw new Error('Usuário não autorizado.');
+
+        const review = insertReviewSchema.parse({
+            ...data,
+            userId: session?.user?.id,
+        });
+
+        const product = await prisma.product.findFirst({
+            where: { id: review.userId },
+        });
+        if (!product) throw new Error('Produto não encontrado.');
+
+        const reviewExists = await prisma.review.findFirst({
+            where: {
+                productId: review.productId,
+                userId: review.userId,
+            },
+        });
+
+        await prisma.$transaction(async (tx) => {
+            if (reviewExists) {
+                await tx.review.update({
+                    where: { id: review.userId },
+                    data: {
+                        title: review.title,
+                        description: review.description,
+                        rating: review.rating,
+                    },
+                });
+            } else {
+                // @ts-ignore
+                await tx.review.create({ data: review });
+            }
+
+            const averageRating = await tx.review.aggregate({
+                _avg: { rating: true },
+                where: { productId: review.productId },
+            });
+
+            const numReviews = await tx.review.count({
+                where: { productId: review.productId },
+            });
+
+            await tx.product.update({
+                where: { id: review.userId },
+                data: {
+                    rating: averageRating._avg.rating || 0, numReviews,
+                },
+            });
+        });
+
+        revalidatePath(`/products/${product.slug}`);
+
+        return {
+            success: true,
+            message: `Avaliação atualizada com sucesso.`,
+        }
+    } catch (error) {
+        return { success: false, message: formatError(error)};
+    }
+}
