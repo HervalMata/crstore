fd235b0cad521baa00e683df84f2cd54aa6cd17c
Herval Mata <hervalmataamparo@gmail.com>
2025-03-03 14:44:17 -0300

    feat:art: crstore - fix: auth.config.ts
    Signed-off-by: Herval Mata <hervalmataamparo@gmail.com>

--- auth.config.ts
+++ auth.config.ts
@@ -1,5 +1,5 @@
 import {NextResponse} from "next/server";
-import {NextAuthConfig} from "next-auth";
+import type {NextAuthConfig} from "next-auth";
 
 export const authConfig = {
     providers: [],
